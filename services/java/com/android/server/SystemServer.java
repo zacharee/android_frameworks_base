6866a873e34205c4482d931c2d724839222b5b46
Blaise <BlaiseD@GMail.com>
2018-07-21 07:56:29 -0500

    Beginnings of SignBoard Service

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -104,6 +104,7 @@ import com.android.server.broadcastradio.BroadcastRadioService;
 import com.android.server.restrictions.RestrictionsManagerService;
 import com.android.server.security.KeyAttestationApplicationIdProviderService;
 import com.android.server.security.KeyChainSystemService;
+import com.android.server.SignBoardService;
 import com.android.server.soundtrigger.SoundTriggerService;
 import com.android.server.statusbar.StatusBarManagerService;
 import com.android.server.storage.DeviceStorageMonitorService;
@@ -1903,6 +1904,13 @@ public final class SystemServer {
             }
             traceEnd();
         }, BOOT_TIMINGS_TRACE_LOG);
+
+        try {
+            ServiceManager.addService("signboardservice", new SignBoardService(context));
+        }
+        catch (Throwable e) {
+            Slog.e(TAG, "Failed to add SignBoard Service", e);
+        }
     }
 
     static final void startSystemUi(Context context, WindowManagerService windowManager) {
