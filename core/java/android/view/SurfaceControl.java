7bea95b091ed1f690d0e52381f63a512d5f5091e
zacharee <zachary.wander@gmail.com>
2018-08-01 19:31:20 +0000

    merge
    Change-Id: Ibd45845c4c4b5ae144fff13018f572e485576669

--- core/java/android/view/SurfaceControl.java
+++ core/java/android/view/SurfaceControl.java
@@ -926,10 +926,6 @@ public class SurfaceControl {
         return nativeScreenshot(getBuiltInDisplay(0), sourceCrop, width, height, 0, 0, true, false, 0);
     }
 
-    public static Bitmap screenshot(int width, int height, Rect sourceCrop) {
-        return nativeScreenshot(getBuiltInDisplay(0), sourceCrop, width, height, 0, 0, true, false, 0);
-    }
-
     private static void screenshot(IBinder display, Surface consumer, Rect sourceCrop,
             int width, int height, int minLayer, int maxLayer, boolean allLayers,
             boolean useIdentityTransform) {
